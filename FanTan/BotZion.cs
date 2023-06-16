641ba842361901f9a7ad4952dc38855b18151645
Thaynan Ribeiro <thaynan.thay@hotmail.com>
2023-06-16 14:43:56 -0300

    Adding RPA on the checkout screen
    Adding RPA on the order completion screen to proceed with filling in the data automatically

--- FanTan/BotZion.cs
+++ FanTan/BotZion.cs
@@ -143,12 +143,51 @@ namespace Projeto
                         Console.WriteLine("Elemento não encontrado após 120 tentativas."); // O elemento não foi encontrado após as tentativas máximas
                     }
 
+                    try // Verificar se uma mensagem de alteração de valor é exibido em tela
+                    {
+                        IWebElement mensagemErro = dr.FindElement(By.XPath("//*[@id=\"contentData\"]/div[3]/div/div/div/div/div[2]/div[1]"));
+                        Console.WriteLine("Mensagem de alteração de valor encontrada! Checkbox marcado");
+                        dr.FindElement(By.XPath("//*[@id=\"contentData\"]/div[3]/div/div/div/div/div[2]/div[2]/label/input")).Click();
+
+                    }
+                    catch (NoSuchElementException)
+                    {
+                        Console.WriteLine("Sem alteração de valor!");
+                    }
+
                     dr.FindElement(By.Id("btBuy")).Click(); // Clica no botão "Comprar"
                     Thread.Sleep(500);
 
-                    dr.FindElement(By.ClassName("firstName")).SendKeys("THAYNAN"); // Insere o nome
+                    while (tentativas < 120) // Aguarda carregamento de um elemento
+                    {
+                        try
+                        {
+                            IWebElement element = dr.FindElement(By.ClassName("firstName"));
+                            elementoPresente = true;
+                            break;
+                        }
+                        catch (NoSuchElementException)
+                        {
+                            Thread.Sleep(1000); // Elemento não encontrado, aguarde 1 segundo e tente novamente
+                            tentativas++;
+                        }
+                    }
+
+                    if (elementoPresente)
+                    {
+                        Console.WriteLine("Elemento encontrado!"); // O elemento está presente na página
+                    }
+                    else
+                    {
+                        Console.WriteLine("Elemento não encontrado após 120 tentativas."); // O elemento não foi encontrado após as tentativas máximas
+                    }
+
+                    Thread.Sleep(3000);
+                    dr.FindElement(By.XPath("//*[@id=\"contentData\"]/div[1]/div[1]/div[3]/div/span[3]/input")).Click(); // Seleciona o campo "Nome"
+                    dr.FindElement(By.XPath("//*[@id=\"contentData\"]/div[1]/div[1]/div[3]/div/span[3]/input")).SendKeys("THAYNAN"); // Insere o nome
                     Thread.Sleep(500);
-                    
+
+                    dr.FindElement(By.ClassName("lastName")).Click(); // Seleciona o campo "Sobrenome"
                     dr.FindElement(By.ClassName("lastName")).SendKeys("RIBEIRO"); // Insere o sobrenome
                     Thread.Sleep(500);
 
@@ -169,6 +208,60 @@ namespace Projeto
 
                     dr.FindElement(By.ClassName("document1")).SendKeys("513.060.560-80"); // Insere o CPF
                     Thread.Sleep(500);
+
+                    dr.FindElement(By.XPath("//*[@id=\"ddcl-paxToItem_Flight_1\"]/span")).Click(); // Abre a lista para selecionar o passageiro
+                    Thread.Sleep(1000);
+
+                    dr.FindElement(By.XPath("//*[@id=\"ddcl-paxToItem_Flight_1-i0\"]")).Click(); // Seleciona o primeiro passageiro
+                    Thread.Sleep(500);
+
+                    dr.FindElement(By.XPath("//*[@id=\"ddcl-creditcardSelector_Flight_1_0\"]/span/span")).Click(); // Abre lista para selecionar a bandeira do cartão
+                    Thread.Sleep(500);
+
+                    dr.FindElement(By.XPath("//*[@id=\"ddcl-creditcardSelector_Flight_1_0-ddw\"]/div/div[3]")).Click(); // Seleciona a bandeira "Visa"
+                    Thread.Sleep(500);
+
+                    dr.FindElement(By.XPath("//*[@id=\"ddcl-parcelsSelector_Flight_1_0\"]/span/span")).Click(); // Abre lista para parcelamento
+                    Thread.Sleep(1000);
+
+                    dr.FindElement(By.XPath("//*[@id=\"ddcl-parcelsSelector_Flight_1_0-ddw\"]/div/div[2]")).Click(); // Seleciona 1 parcela
+                    Thread.Sleep(1000);
+
+                    dr.FindElement(By.XPath("//*[@id=\"creditcardNumber_Flight_1_0\"]")).SendKeys("4444444444444444"); // Preenche campo "Número do cartão"
+                    Thread.Sleep(1000);
+
+                    dr.FindElement(By.XPath("//*[@id=\"mpCreditcardNameTxt_Flight_1_0\"]")).Click();
+                    dr.FindElement(By.XPath("//*[@id=\"mpCreditcardNameTxt_Flight_1_0\"]")).SendKeys("THAYNAN RIBEIRO"); // Preenche campo "Nome do titular"
+                    Thread.Sleep(1000);
+
+                    /*dr.FindElement(By.Id("mpCreditcardDocumentTxt")).Click();
+                    dr.FindElement(By.Id("mpCreditcardDocumentTxt")).SendKeys("513.060.560-80"); // Preenche campo "CPF" -------- ESTÁ COM PROBLEMA NO LAYOUT, PRECISA AJUSTAR O DISPLAY: BLOCK;
+                    Thread.Sleep(1000);*/
+
+                    dr.FindElement(By.Id("mpCreditcardMonthTxt_Flight_1_0")).Click();
+                    dr.FindElement(By.Id("mpCreditcardMonthTxt_Flight_1_0")).SendKeys("05"); // Preenche campo "Mês Validade"
+                    Thread.Sleep(1000);
+
+                    dr.FindElement(By.Id("mpCreditcardYearTxt_Flight_1_0")).Click();
+                    dr.FindElement(By.Id("mpCreditcardYearTxt_Flight_1_0")).SendKeys("2025"); // Preenche campo "Ano Validade"
+                    Thread.Sleep(1000);
+
+                    dr.FindElement(By.Id("mpCreditcardSecurityCodeTxt_Flight_1_0")).Click();
+                    dr.FindElement(By.Id("mpCreditcardSecurityCodeTxt_Flight_1_0")).SendKeys("123"); // Preenche campo "Cód. Segurança"
+                    Thread.Sleep(1000);
+
+                    dr.FindElement(By.XPath("//*[@id=\"divCreditcard_Flight_1_0\"]/span[6]/span[1]/input")).Click();
+                    dr.FindElement(By.XPath("//*[@id=\"divCreditcard_Flight_1_0\"]/span[6]/span[1]/input")).SendKeys("15050-000"); // Preenche campo "Código Postal"
+                    Thread.Sleep(1000);
+
+                    dr.FindElement(By.XPath("//*[@id=\"divCreditcard_Flight_1_0\"]/span[6]/span[3]/input")).Click();
+                    dr.FindElement(By.XPath("//*[@id=\"divCreditcard_Flight_1_0\"]/span[6]/span[3]/input")).SendKeys("123"); // Preenche campo "Número"
+                    Thread.Sleep(1000);
+
+                    dr.FindElement(By.XPath("//*[@id=\"contentData\"]/div[1]/div[9]/div[2]/label[1]/input")).Click(); // Aceita os termos do contrato
+                    Thread.Sleep(1000);
+
+
                 }
                 catch (Exception e)
                 {
